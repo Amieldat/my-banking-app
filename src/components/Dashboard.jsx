1912dd651e98c677372fce03cd2bd5ed10b0ca5a
Epi Casaljay <epifanio.81546@gmail.com>
2023-09-03 00:41:52 +0800

    Implement separate interfaces for admin and users

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,30 +1,45 @@
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useLocalStorage } from '../hooks'
-import { AccountList, AddAccount, Deposit, Transfer, Withdraw } from '../sub-components'
+import { AccountList, AddAccount, AddExpense, Budget, Deposit, Transfer, Withdraw } from '../sub-components'
 
 export const Dashboard = () => {
     const [loggedIn, setLoggedIn] = useLocalStorage("loggedIn")
     const navigate = useNavigate()
-    const [modalToggle, setModalToggle] = useState(false)
-
+    const [accountModalToggle, setAccountModalToggle] = useState(false)
+    const [expenseModalToggle, setExpenseModalToggle] = useState(false)
+    
     useEffect(() => {
         if (loggedIn == "") navigate("/")
     }, [loggedIn]) 
 
     const [users, setUsers] = useLocalStorage("users", [])
-
+    const isAdmin = users.find(({ username }) => loggedIn == username)?.role == "admin"
 
     return (
         <>
             <button onClick={() => setLoggedIn("")}>Log Out</button>
-            dashboard
-            <button onClick={() => setModalToggle(!modalToggle)}>Add Account</button>
-            <AccountList users={users} />
-            <Deposit users={users} setUsers={setUsers} /> 
+            Dashboard
+            {isAdmin 
+                ? (
+                    <>
+                        Admin
+                        <button onClick={() => setAccountModalToggle(!accountModalToggle)}>Add Account</button>
+                        <AccountList users={users} />
+                        <AddAccount users={users} />
+                    </>
+                )
+                : (
+                    <>
+                        User
+                        <button onClick={() => setExpenseModalToggle(!expenseModalToggle)}>Add Expense</button>
+                        <Budget users={users} loggedIn={loggedIn} />
+                        <AddExpense users={users} setUsers={setUsers} loggedIn={loggedIn} modalToggle={expenseModalToggle} />
+                    </>
+                )}
+            <Deposit users={users} setUsers={setUsers} />
             <Withdraw users={users} setUsers={setUsers} />
             <Transfer users={users} setUsers={setUsers} />
-            <AddAccount users={users} setUsers={setUsers} modalToggle={modalToggle} />
         </>
     )
 }
